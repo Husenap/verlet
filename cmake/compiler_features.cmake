23246da6de2d858b3da2aa70cf04257816fef81f
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-07-09 21:56:02 +0200

    added openmp flag

--- cmake/compiler_features.cmake
+++ cmake/compiler_features.cmake
@@ -2,4 +2,6 @@ if(NOT TARGET compiler_features)
     add_library(compiler_features INTERFACE)
 
     target_compile_features(compiler_features INTERFACE cxx_std_20)
+
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /openmp")
 endif()
